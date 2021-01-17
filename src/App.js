a1b2370c24db176df1ec1918c599b23b28910064
Allen <jsc6879@gmail.com>
2021-01-17 13:10:08 +0900

    First Commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component{
+    render(){
+        return(
+            <div className="gray-backgroud">
+                <img src={logo} lat="logo" />
+                <h2> lgosdkfjsldk</h2>
+
+            </div>
+        )
+    }
+}
+
+export default App;
---
+++ src/Hello.js
@@ -0,0 +1,8 @@
+import react from 'react';
+
+function Hello(){
+    return <div>안녕하세요 </div>
+
+}
+
+export default Hello;
